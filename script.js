bf4addded034f5c456785b97e8c5ac31db0a2dd3
Roman <stoleru.roman@gmail.com>
2023-01-23 23:25:28 -0300

    add_content

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-let costumInform = document.querySelector("[data-button-customerInformation]")
+let costumInform = document.querySelector("[data-onOff-costumerInformation]")
+costumInform.addEventListener("click", () => {
+    let showCostumerInformation = document.querySelector(["data-onOff-costumerInformation"])
+    if (showCostumerInformation.style.display === "none") {
+        showCostumerInformation.style.display = "block"
+    }
+    else {
+        showCostumerInformation.style.display = "none"
+    }
+})
