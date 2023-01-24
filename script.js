a02b5e572220d01cc380ebca133308b086e3d43e
Roman <stoleru.roman@gmail.com>
2023-01-24 00:30:21 -0300

    add_content

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-let costumInform = document.getElementById("data-button-customerInformation")
+let costumInform = document.getElementById("customerInformation")
 costumInform.addEventListener("click", () => {
-    const showInformation = document.getElementById("data-onOff-costumerInformation")
+    const showInformation = document.getElementById("onOff-costumerInformation")
+    if (showInformation.style.display === "none") {
+        showInformation.style.display = "block"
+
+    }
+    else {
+        showInformation.style.display = "none"
+    }
+})
+
+let pdiSector = document.getElementById("pdiSector")
+pdiSector.addEventListener("click", () => {
+    const showPdiSector = document.getElementById("onOff-pdiSector")
     if (showInformation.style.display === "none") {
         showInformation.style.display = "block"
 
