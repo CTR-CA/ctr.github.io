a74fcb68be9bc3d6581716e7daf102e7289cf8d3
Roman <stoleru.roman@gmail.com>
2023-01-28 19:21:07 -0300

    add_content

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 let costumInform = document.getElementById("customerInformation")
 costumInform.addEventListener("click", () => {
     const showInformation = document.getElementById("onOff-costumerInformation")
+
+
     if (showInformation.style.display === "none") {
         showInformation.style.display = "block"
 
     }
     else {
         showInformation.style.display = "none"
+
     }
+
+
+
 })
 
 let pdiSector = document.getElementById("pdiSector")
 pdiSector.addEventListener("click", () => {
     const showPdiSector = document.getElementById("onOff-pdiSector")
+
+
     if (showPdiSector.style.display === "none") {
         showPdiSector.style.display = "block"
 
@@ -25,6 +33,8 @@ pdiSector.addEventListener("click", () => {
 let settingSector = document.getElementById("settingSector")
 settingSector.addEventListener("click", () => {
     const showSettingSector = document.getElementById("onOff-settingsSector")
+
+
     if (showSettingSector.style.display === "none") {
         showSettingSector.style.display = "block"
 
