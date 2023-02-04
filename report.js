97248a97f26e79b56443b70d1311e0d6ab159459
Roman <stoleru.roman@gmail.com>
2023-02-04 16:20:30 -0300

    add_content

--- report.js
+++ report.js
@@ -2,8 +2,25 @@
 
 const button = document.getElementById('report');
 button.addEventListener('click', generatePDF);
+
 function generatePDF() {
-    const element = document.getElementById('invoice');
-    html2pdf().from(element).save();
+    window.jsPDF = window.jspdf.jsPDF;
+    let doc = new jsPDF();
+
+    const element = document.getElementById('contentToPrint');
+
+    doc.html(element, {
+        callback: function (doc) {
+            doc.save('document-html.pdf');
+        },
+        margin: [10, 10, 10, 10],
+        autoParging: 'text',
+        x: 0,
+        y: 0,
+        width: 190,
+        windowWidth: 675
+    })
+
 
 }
+//html2pdf().from(element).save();
