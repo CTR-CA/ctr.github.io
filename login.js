cde0c65503fa3d284d75338fc8d1d83b656d2dc5
Roman <stoleru.roman@gmail.com>
2023-02-08 23:23:36 -0300

    add_content_check_js

--- login.js
+++ login.js
@@ -1,26 +1,39 @@
 
-let login = document.getElementById("log__in")
-login.addEventListener("click", () => {
 
-    const userData = document.getElementById("userName").value.toUpperCase()
-    const userDataWrite = document.getElementById("userData")
-    userDataWrite.innerHTML = `NAME:${userData}`
+function loginDone() {
+    let login = document.getElementById("log__in")
+    login.addEventListener("click", () => {
 
-    const d = new Date();
-    document.getElementById("data").innerHTML = d;
+        const userData = document.getElementById("userName").value.toUpperCase()
+        const userDataWrite = document.getElementById("userData")
+        userDataWrite.innerHTML = `NAME:${userData}`
 
-    let showBody = document.getElementById('on__off__body__style')
-    console.log("")
+        const dateTime = new Date();
+        document.getElementById("dateTime").innerHTML = dateTime;
 
-    if (showBody.style.display === "none") {
-        showBody.style.display = "block"
-    }
-    if (showBody.style.display = "block") {
-        let logInOff = document.getElementById('logInscreenDiv')
-        logInOff.style.display = "none"
+        let showBody = document.getElementById('on__off__body__style')
+        console.log("")
 
+        if (showBody.style.display === "none") {
+            showBody.style.display = "block"
+        }
+        if (showBody.style.display = "block") {
+            let logInOff = document.getElementById('logInscreenDiv')
+            logInOff.style.display = "none"
+
+        }
+        else {
+            showBody.style.display = "none"
+        }
+    })
+
+}
+checkLogin()
+function checkLogin() {
+    if (userData != "") {
+        loginDone()
     }
     else {
-        showBody.style.display = "none"
+        alert("try again")
     }
-})
+}
