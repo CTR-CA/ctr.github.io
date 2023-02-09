dd03d09d099dfb8de0233032d4aaf40f95221dbf
Roman <stoleru.roman@gmail.com>
2023-02-08 23:41:21 -0300

    add_content

--- login.js
+++ login.js
@@ -1,39 +1,47 @@
+let login = document.getElementById("log__in")
+login.addEventListener("click", () => {
 
+    const userData = document.getElementById("userName").value.toUpperCase()
 
-function loginDone() {
-    let login = document.getElementById("log__in")
-    login.addEventListener("click", () => {
-
-        const userData = document.getElementById("userName").value.toUpperCase()
-        const userDataWrite = document.getElementById("userData")
-        userDataWrite.innerHTML = `NAME:${userData}`
-
-        const dateTime = new Date();
-        document.getElementById("dateTime").innerHTML = dateTime;
-
-        let showBody = document.getElementById('on__off__body__style')
-        console.log("")
-
-        if (showBody.style.display === "none") {
-            showBody.style.display = "block"
-        }
-        if (showBody.style.display = "block") {
-            let logInOff = document.getElementById('logInscreenDiv')
-            logInOff.style.display = "none"
-
-        }
-        else {
-            showBody.style.display = "none"
-        }
-    })
-
-}
-checkLogin()
-function checkLogin() {
-    if (userData != "") {
-        loginDone()
+    if (!userData == ``) {
+        dataDone()
     }
     else {
         alert("try again")
     }
+
+
+})
+
+
+function dataDone() {
+    const userData = document.getElementById("userName").value.toUpperCase()
+    const userDataWrite = document.getElementById("userData")
+    userDataWrite.innerHTML = `NAME:${userData}`
+
+    const dateTime = new Date();
+    document.getElementById("dateTime").innerHTML = dateTime;
+
+    let showBody = document.getElementById('on__off__body__style')
+    console.log("done")
+
+    if (showBody.style.display === "none") {
+        showBody.style.display = "block"
+    }
+    if (showBody.style.display = "block") {
+        let logInOff = document.getElementById('logInscreenDiv')
+        logInOff.style.display = "none"
+
+    }
+    else {
+        showBody.style.display = "none"
+    }
 }
+
+
+
+
+
+
+
+
