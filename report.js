cad9a510449499ce429c6abbc5814ada73f0dbf1
RomanStoleru <stoleru.roman@gmail.com>
2023-02-12 16:08:49 -0600

    add_content

--- report.js
+++ report.js
@@ -1,8 +1,34 @@
 const button = document.getElementById('report');
 button.addEventListener('click', () => {
-
+    afterPrintReport()
     generatePDF()
 });
+
+function afterPrintReport() {
+
+    const afterprint1 = document.querySelector("onOff-costumerInformation")
+
+    if (afterprint1.style.display === "none") {
+        afterprint1.style.display = "block"
+
+    }
+
+    const afterprint2 = document.querySelector("onOff-pdiSector")
+
+    if (afterprint2.style.display === "none") {
+        afterprint2.style.display = "block"
+
+    }
+
+    const afterprint3 = document.querySelector("onOff-settingsSector")
+
+    if (afterprint3.style.display === "none") {
+        afterprint3.style.display = "block"
+
+    }
+
+
+}
 
 function generatePDF() {
     window.jsPDF = window.jspdf.jsPDF;
